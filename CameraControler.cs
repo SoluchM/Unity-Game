653fe6d06b29d057f75a1586b010465d131e3f09
Mateusz <mateuszsoluch@gmail.com>
2023-10-28 19:45:20 +0200

    CameraControler fixed

--- CameraControler.cs
+++ CameraControler.cs
@@ -6,10 +6,18 @@ public class CameraControler : MonoBehaviour
 {
     [SerializeField]private Transform player;
 
-
     // Update is called once per frame
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, 3, -15);
+        float hold = player.position.y;
+        if (hold>3)
+        {
+            transform.position = new Vector3(player.position.x, player.position.y, -17);
+        }
+        else
+        {
+            transform.position = new Vector3(player.position.x, 3, -17);
+        }
+
     }
 }
